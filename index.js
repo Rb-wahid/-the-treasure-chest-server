23cd7075eb54311da3d0729a91d4962003652b1d
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-04-30 18:26:44 +0600

    database connected

--- index.js
+++ index.js
@@ -7,5 +7,24 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ij5kp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+
+    const inventoryCollection = await client
+      .db("service")
+      .collection("products");
+  } finally {
+  }
+};
+
+run().catch(console.dir);
 
 app.listen(port, () => console.log("listening at ", port));
